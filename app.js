170f98cafeb2f419375e103090859a995a007108
Stephane <sgobin@campus-eni.fr>
2025-01-29 18:28:06 +0100

    us001-us005-us006-us007

--- app.js
+++ app.js
@@ -9,47 +9,10 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-let issues = [
-  { id: 1, titre: "issue 1", description: "description 1" },
-  { id: 2, titre: "issue 2", description: "description 2" },
-  { id: 3, titre: "issue 3", description: "description 3" },
-];
+var indexRouter = require("./routes/indexRouter");
+var ticketsRouter = require("./routes/ticketsRouter");
 
-let idx = 4;
-
-app.get(["/", "/issues"], (req, res) => {
-  res.render("index", { issues });
-});
-
-app.post("/issues", (req, res) => {
-  const { titre, description } = req.body;
-  issues.push({ id: idx++, titre, description });
-  res.redirect("/");
-});
-
-app.get("/issues/:id/modifier", (req, res) => {
-  const { id } = req.params;
-  const issue = issues.find((issue) => issue.id == id);
-  res.render("modifier-issue", { issue });
-});
-
-app.post("/issues/:id/modifier", (req, res) => {
-  const { id } = req.params;
-  const { titre, description } = req.body;
-  let index = issues.findIndex((issue) => issue.id == id);
-  issues[index] = {
-    id: id,
-    titre: titre || issues[index].titre,
-    description: description || issues[index].description,
-  };
-  res.redirect("/");
-});
-
-app.get("/issues/:id/supprimer", (req, res) => {
-  const { id } = req.params;
-  let index = issues.findIndex((issue) => issue.id == id);
-  issues.splice(index, 1);
-  res.redirect("/");
-});
+app.use("/", indexRouter);
+app.use("/tickets", ticketsRouter);
 
 app.listen(3000);
---
+++ routes/ticketsRouter.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var ticketsService = require("../services/ticketsService");
+
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { tickets: ticketsService.findAllTickets() });
+});
+
+router.post("/", (req, res) => {
+  const { titre, description } = req.body;
+
+  ticketsService.addTicket(titre, description);
+
+  res.redirect("/");
+});
+
+router.get("/:id/modifier", (req, res) => {
+  const { id } = req.params;
+  const ticket = ticketsService.findTicketById(id);
+  res.render("modifier-ticket", { ticket });
+});
+
+router.post("/:id/modifier", (req, res) => {
+  const { id } = req.params;
+  const { titre, description } = req.body;
+  ticketsService.updateTicket(id, titre, description);
+  res.redirect("/");
+});
+
+router.get("/:id/supprimer", (req, res) => {
+  const { id } = req.params;
+  ticketsService.deleteTicket(id);
+  res.redirect("/");
+});
+
+module.exports = router;
