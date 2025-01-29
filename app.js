50732e96b843a9d9c9b70c8a2269763109e3fabb
Stephane <sgobin@campus-eni.fr>
2025-01-29 18:35:13 +0100

    us004

--- app.js
+++ app.js
@@ -15,4 +15,14 @@ var ticketsRouter = require("./routes/ticketsRouter");
 app.use("/", indexRouter);
 app.use("/tickets", ticketsRouter);
 
+app.use((req, res, next) => {
+  res.status(404).render("erreur404");
+});
+
+// Middleware de gestion d'erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("erreur", { titre: "Erreur interne", erreur: err });
+});
+
 app.listen(3000);
